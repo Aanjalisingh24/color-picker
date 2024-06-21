186c498016cd3e1ceca958297a5ea17c42663c9f
Aanjalisingh24 <aanjali23@navgurukul.org>
2024-06-21 17:18:44 +0530

    inicial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function turnonwhite(){
+    document.getElementById("daba").style.backgroundColor="#e0e0e0"
+    document.getElementById("color").textContent="#e0e0e0"
+}
+function turnongreen(){
+    document.getElementById("daba").style.backgroundColor="#6fcf97"
+    document.getElementById("color").textContent="#6fcf97"
+}
+function turnonblue(){
+    document.getElementById("daba").style.backgroundColor="#56ccf2"
+    document.getElementById("color").textContent="#56ccf2"
+}
+function turnonpurple(){
+    document.getElementById("daba").style.backgroundColor="#bb6bd9"
+    document.getElementById("color").textContent="#bb6bd9"
+}
